6ad479bf8d20c468c9f7f4ff9af599331476c492
furkansimsekli <simseklifurkan0@gmail.com>
2024-02-23 01:48:01 +0300

    base.faq: init faq model and migration

--- oytwebsite/base/models.py
+++ oytwebsite/base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Faq(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.question
