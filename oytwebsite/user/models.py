fd1cda6018d491d6c90f8868c4f46b54c6c59f26
furkansimsekli <simseklifurkan0@gmail.com>
2024-02-23 23:29:38 +0300

    user.model: init member model
    I was quite indecisive about whether I should account every member
    as a user on the platform or I should just store them as standalone.
    Our platform don't have any user capabilities right now except admin
    priviliges. Maybe in the long term, there could be something to do,
    but right now let's keep things as simple as possible.

--- oytwebsite/user/models.py
+++ oytwebsite/user/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    DEGREE_CHOICES = (
+        ('Hazırlık', 'Hazırlık'),
+        ('1. Sınıf', '1. Sınıf'),
+        ('2. Sınıf', '2. Sınıf'),
+        ('3. Sınıf', '3. Sınıf'),
+        ('4. Sınıf', '4. Sınıf'),
+        ('5. Sınıf', '5. Sınıf'),
+        ('6. Sınıf', '6. Sınıf'),
+        ('Yüksek Lisans', 'Yüksek Lisans'),
+        ('Doktora', 'Doktora'),
+        ('Mezun', 'Mezun')
+    )
+
+    GROUP_CHAT_PLATFORM_CHOICES = (
+        ('Signal', 'Signal'),
+        ('Telegram', 'Telegram'),
+        ('WhatsApp', 'WhatsApp')
+    )
+
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    student_id = models.CharField(max_length=15)
+    department = models.CharField(max_length=64)
+    degree = models.CharField(max_length=15, choices=DEGREE_CHOICES)
+    email = models.EmailField()
+    mobile_number = models.CharField(max_length=15)
+    group_chat_platform = models.CharField(max_length=8, choices=GROUP_CHAT_PLATFORM_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.student_id
