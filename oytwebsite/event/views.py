2ba05df252e97be3449ee78ef2bf65d524a631da
furkansimsekli <simseklifurkan0@gmail.com>
2024-02-24 01:34:09 +0300

    event: add view, and html for events and courses

--- oytwebsite/event/views.py
+++ oytwebsite/event/views.py
@@ -1,9 +1,20 @@
+from django.shortcuts import render
 from django.views import View
+
+from .models import Event, Course
 
 
 class EventListView(View):
     def get(self, request):
-        pass
+        events = Event.objects.filter(is_active=True)
+        courses = Course.objects.filter(is_active=True)
 
-    def post(self, request):
-        pass
+        # SQLite3 doesn't support ArrayField. Below, there is a simple trick to get over this problem.
+        for course in courses:
+            course.pre_requisites = course.pre_requisites.split(';') if course.pre_requisites else []
+
+        ctx = {
+            'events': events,
+            'courses': courses,
+        }
+        return render(request, 'event/event_list.html', ctx)
