44b3310871eeb0bce342f019ac64ff0be8a0cb48
furkansimsekli <simseklifurkan0@gmail.com>
2024-02-24 01:15:43 +0300

    fix: incorrect media urlpattern location

--- oytwebsite/event/urls.py
+++ oytwebsite/event/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -7,6 +5,3 @@ from . import views
 urlpatterns = [
     path('', views.EventListView.as_view(), name='event-list')
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
