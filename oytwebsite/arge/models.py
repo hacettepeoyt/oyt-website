5b361028f3b925f26d0a8bd3dd3f48feb09ecbaf
furkansimsekli <simseklifurkan0@gmail.com>
2024-03-12 02:45:17 +0300

    fix: use higher threshold for resizing uploaded images
    I realized that it's just a temporary solution to have files that
    are smaller in terms of size. I will try to add an actual compressing
    in the future.
    
    Fixes #6

--- oytwebsite/arge/models.py
+++ oytwebsite/arge/models.py
@@ -17,7 +17,7 @@ class Project(models.Model):
         super().save()
         img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
+        if img.height > 1000 or img.width > 1000:
+            output_size = (1000, 1000)
             img.thumbnail(output_size)
             img.save(self.image.path)
--- oytwebsite/event/models.py
+++ oytwebsite/event/models.py
@@ -20,8 +20,8 @@ class Event(models.Model):
         super().save()
         img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
+        if img.height > 1000 or img.width > 1000:
+            output_size = (1000, 1000)
             img.thumbnail(output_size)
             img.save(self.image.path)
 
@@ -45,7 +45,7 @@ class Course(models.Model):
         super().save()
         img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
+        if img.height > 1000 or img.width > 1000:
+            output_size = (1000, 1000)
             img.thumbnail(output_size)
             img.save(self.image.path)
