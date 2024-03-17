ef0a02423eb52f55c1dd4b4e80f0aafa2811e38f
furkansimsekli <simseklifurkan0@gmail.com>
2024-03-17 21:38:31 +0300

    settings: use debug mode by default

--- oytwebsite/oytwebsite/settings.py
+++ oytwebsite/oytwebsite/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = CONFIG['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = CONFIG['DEBUG']
+DEBUG = CONFIG.get('DEBUG', True)
 
 ALLOWED_HOSTS = CONFIG.get('ALLOWED_HOSTS', ['*'])
 
