82430c5bf3a0fb808db4f429b88f1c828805ad6d
furkansimsekli <simseklifurkan0@gmail.com>
2024-08-11 01:44:10 +0300

    arge.idea: remove crispy, localize error messages

--- oytwebsite/arge/forms.py
+++ oytwebsite/arge/forms.py
@@ -1,74 +1,81 @@
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Div, HTML, Field, Submit, Fieldset, Layout
 from django import forms
 
 
 class IdeaForm(forms.Form):
-    first_name = forms.CharField(max_length=32, required=True)
-    last_name = forms.CharField(max_length=32, required=True)
-    email = forms.EmailField(required=True)
-    project_title = forms.CharField(max_length=64, required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_method = 'POST'
-        self.helper.layout = get_idea_form_layout()
-        self.helper.form_show_labels = False
+    first_name = forms.CharField(
+        label='İsim',
+        max_length=32,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control bg-white border-left-0 border-md',
+            'placeholder': 'İsim'
+        }),
+        error_messages={
+            'required': 'İsmini boş bırakamazsın!',
+            'max_length': 'Eğer 32 karakterden uzunsa ismin, kısalt lütfen :)'
+        }
+    )
+    last_name = forms.CharField(
+        label='Soyisim',
+        max_length=32,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control bg-white border-left-0 border-md',
+            'placeholder': 'Soyisim'
+        }),
+        error_messages={
+            'required': 'Soyismini boş bırakamazsın!',
+            'max_length': 'Eğer 32 karakterden uzunsa soyismin, kısalt lütfen :)'
+        }
+    )
+    email = forms.EmailField(
+        label='Mail Adresi',
+        max_length=320,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control bg-white border-left-0 border-md',
+            'placeholder': 'Eposta Adresi'
+        }),
+        error_messages={
+            'required': 'Eposta adresini boş bırakamazsın!',
+            'max_length': '320 karakterden uzun eposta adresi mi olur ya?',
+            'invalid': 'Geçersiz bir eposta adresi girdin!'
+        }
+    )
+    project_title = forms.CharField(
+        label='Proje İsmi',
+        max_length=64,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control bg-white border-left-0 border-md',
+            'placeholder': 'Proje İsmi'
+        }),
+        error_messages={
+            'required': 'Proje ismini boş bırakamazsın!',
+            'max_length': 'Proje ismi 64 karakterden fazla olamaz!'
+        }
+    )
+    message = forms.CharField(
+        label='Mesajınız',
+        widget=forms.Textarea(attrs={
+            'class': 'form-control bg-white border-left-0 border-md',
+            'placeholder': 'Mesajınız',
+            'rows': 7
+        }),
+        error_messages={
+            'required': 'Bir şeyler söyle de anlayalım ne istediğini =)',
+        }
+    )
 
     def clean(self):
         cleaned_data = super().clean()
 
         # Format with single space separation, make them Title Case
-        cleaned_data['first_name'] = ' '.join(cleaned_data['first_name'].split()).title()
-        cleaned_data['last_name'] = ' '.join(cleaned_data['last_name'].split()).title()
-        cleaned_data['project_title'] = ' '.join(cleaned_data['project_title'].split()).title()
+        if cleaned_data.get('first_name'):
+            cleaned_data['first_name'] = ' '.join(cleaned_data['first_name'].split()).title()
 
-        cleaned_data['email'] = cleaned_data['email'].lower()
+        if cleaned_data.get('last_name'):
+            cleaned_data['last_name'] = ' '.join(cleaned_data['last_name'].split()).title()
+
+        if cleaned_data.get('project_title'):
+            cleaned_data['project_title'] = ' '.join(cleaned_data['project_title'].split()).title()
+
+        if cleaned_data.get('email'):
+            cleaned_data['email'] = cleaned_data['email'].lower()
         return cleaned_data
-
-
-def get_idea_form_layout():
-    return Layout(
-        Fieldset(
-            '',
-            get_field_with_icon(icon_class='bi bi-person',
-                                field_name='first_name',
-                                placeholder='İsim'),
-            get_field_with_icon(icon_class='bi bi-person',
-                                field_name='last_name',
-                                placeholder='Soyisim'),
-            get_field_with_icon(icon_class='bi bi-envelope',
-                                field_name='email',
-                                placeholder='Mail Adresi'),
-            get_field_with_icon(icon_class='bi bi-pencil',
-                                field_name='project_title',
-                                placeholder='Proje Adı'),
-            get_field_with_icon(icon_class='bi bi-send',
-                                field_name='message',
-                                placeholder='Proje ilgili açıklamarı buraya yazabilirsiniz!'),
-        ),
-        Div(
-            Div(
-                css_class='col-sm-2 col-form-label text-end'
-            ),
-            Submit('submit', 'Gönder', css_id='signup-btn', css_class='col-sm-2'),
-            css_class='form-group row d-flex align-items-center',
-        )
-    )
-
-
-def get_field_with_icon(icon_class, field_name, placeholder=None):
-    return Div(
-        Div(
-            HTML(
-                f"""
-                    <i class="{icon_class} fs-2" style="color: #ff4100;"></i>
-                """
-            ),
-            css_class='col-sm-2 col-form-label'
-        ),
-        Field(field_name, wrapper_class='pt-3 col-sm-10', placeholder=placeholder),
-        css_class='form-group row d-flex align-items-center',
-    )
