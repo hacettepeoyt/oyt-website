9d79260e7fc28b5b085ac82f67b05fa0d4a30feb
furkansimsekli <simseklifurkan0@gmail.com>
2024-08-11 02:33:37 +0300

    settings: set default secret key
    Why force developers to include a secret key in their config?

--- oytwebsite/oytwebsite/settings.py
+++ oytwebsite/oytwebsite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = CONFIG['SECRET_KEY']
+SECRET_KEY = CONFIG.get('SECRET_KEY', 'unbowed-unbent-unbroken')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = CONFIG.get('DEBUG', True)
