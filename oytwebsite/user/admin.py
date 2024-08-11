fd6550033ffe83b8286ecff95616fdc25608a141
furkansimsekli <simseklifurkan0@gmail.com>
2024-08-11 12:33:43 +0300

    user.admin: add new actions
    - Mark the selected members as added to group chat
    - Mark the selected members as not added to group chat
    - Download the selected members that are not in the groups as CSV
    - Download the selected members that are not in the groups as VCF

--- oytwebsite/user/admin.py
+++ oytwebsite/user/admin.py
@@ -54,9 +54,36 @@ def download_vcf(modeladmin, request, queryset):
     return response
 
 
+@admin.action(description='Mark the selected members as added to group chat')
+def mark_as_added_to_group_chat(modeladmin, request, queryset):
+    queryset.update(is_in_group_chat=True)
+
+
+@admin.action(description='Mark the selected members as not added to group chat')
+def mark_as_not_added_to_group_chat(modeladmin, request, queryset):
+    queryset.update(is_in_group_chat=False)
+
+
+@admin.action(description='Download the selected members that are not in the groups as CSV')
+def download_csv_not_in_group_chat(modeladmin, request, queryset):
+    queryset = queryset.filter(is_in_group_chat=False)
+    return download_csv(modeladmin, request, queryset)
+
+
+@admin.action(description='Download the selected members that are not in the groups as VCF')
+def download_vcf_not_in_group_chat(modeladmin, request, queryset):
+    queryset = queryset.filter(is_in_group_chat=False)
+    return download_vcf(modeladmin, request, queryset)
+
+
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ['student_id', 'first_name', 'last_name', 'department', 'created_at']
-    actions = [download_csv, download_vcf]
+    list_display = ['student_id', 'first_name', 'last_name', 'department', 'is_in_group_chat', 'created_at']
+    actions = [download_csv,
+               download_csv_not_in_group_chat,
+               download_vcf,
+               download_vcf_not_in_group_chat,
+               mark_as_added_to_group_chat,
+               mark_as_not_added_to_group_chat]
 
 
 admin.site.register(Member, MemberAdmin)
