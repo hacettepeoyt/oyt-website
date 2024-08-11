ba630a746dab841ddf986f82e6794a024e00f0c3
furkansimsekli <simseklifurkan0@gmail.com>
2024-08-11 03:46:07 +0300

    utils: send message to logger when matrix creds are missing

--- oytwebsite/utils.py
+++ oytwebsite/utils.py
@@ -1,12 +1,21 @@
 import json
+import logging
 
 import requests
 from django.conf import settings
 
+logger = logging.getLogger(__name__)
+
 
 def send_message_to_admin_room(message):
-    MATRIX_ADMIN_ROOM = settings.CONFIG['MATRIX_ADMIN_ROOM']
-    MATRIX_ACCESS_TOKEN = settings.CONFIG['MATRIX_ACCESS_TOKEN']
+    MATRIX_ADMIN_ROOM = settings.CONFIG.get('MATRIX_ADMIN_ROOM')
+    MATRIX_ACCESS_TOKEN = settings.CONFIG.get('MATRIX_ACCESS_TOKEN')
+
+    if not MATRIX_ADMIN_ROOM or not MATRIX_ACCESS_TOKEN:
+        logger.warning('Matrix configurations are not fully set. '
+                       'Sending message to logger instead of matrix admin room.')
+        logger.info(message)
+        return
 
     url = (f'https://matrix.org/_matrix/client/r0/rooms/{MATRIX_ADMIN_ROOM}/send'
            f'/m.room.message?access_token={MATRIX_ACCESS_TOKEN}')
