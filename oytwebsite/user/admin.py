367a5d5e185e4de4b5e556919d4c2133a6a21f41
furkansimsekli <simseklifurkan0@gmail.com>
2024-08-11 12:17:27 +0300

    user.admin: add an option to download as vcf

--- oytwebsite/user/admin.py
+++ oytwebsite/user/admin.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from user.models import Member, BoardMember
 
 
-@admin.action(description="Download the selected members as CSV")
+@admin.action(description='Download the selected members as CSV')
 def download_csv(modeladmin, request, queryset):
     response = HttpResponse(
         content_type='text/csv',
@@ -30,9 +30,33 @@ def download_csv(modeladmin, request, queryset):
     return response
 
 
+@admin.action(description='Download the selected members as VCF')
+def download_vcf(modeladmin, request, queryset):
+    response = HttpResponse(
+        content_type='text/x-vcard',
+        headers={'Content-Disposition': f'attachment; filename="members_{date.today()}.vcf"'}
+    )
+
+    vcf_lines = []
+    current_year = date.today().year % 100
+
+    for member in queryset:
+        vcf_lines.append('BEGIN:VCARD')
+        vcf_lines.append('VERSION:3.0')
+        vcf_lines.append(f'FN:OYT{current_year}{member.group_chat_platform[0]} {member.first_name} {member.last_name}')
+        vcf_lines.append(f'ORG:{member.department}')
+        vcf_lines.append(f'TEL;TYPE=cell:{member.mobile_number}')
+        vcf_lines.append(f'EMAIL:{member.email}')
+        vcf_lines.append(f'NOTE:{member.student_id} - {member.degree}')
+        vcf_lines.append('END:VCARD')
+
+    response.content = "\n".join(vcf_lines)
+    return response
+
+
 class MemberAdmin(admin.ModelAdmin):
     list_display = ['student_id', 'first_name', 'last_name', 'department', 'created_at']
-    actions = [download_csv]
+    actions = [download_csv, download_vcf]
 
 
 admin.site.register(Member, MemberAdmin)
